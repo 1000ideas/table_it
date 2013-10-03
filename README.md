e721a391475bbee33360a150c1d5c6783a78384a
Ewelin <ewelina@1000i.pl>
2013-10-03 11:09:01 +0200

    installation instructions, translations, routes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-redmine-table-it
+Redmine Table It plugin
 ================
+1. add plugin to your redmine's plugins directory
+2. run 
+```ruby
+rails g table_it 
+```
+3. restart application
+
--- init.rb
+++ init.rb
@@ -1,10 +1,14 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_table_it do
+Redmine::Plugin.register :table_it do
   name 'TableIt'
-  author 'Ewelina Lis'
+  author 'Ewelina Mijal'
   description 'Watch your issues right from redmine home page'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+  
+  project_module :table_it do
+      permission :home, { :table_it => [:table_it] },:public => true
+  end
 end
