55c457ae9ddd1c471b881351b0c197bda1b90e96
Bartek Bulat <bartek@1000i.pl>
2014-01-16 15:38:08 +0100

    Bump version

--- init.rb
+++ init.rb
@@ -7,13 +7,12 @@ Redmine::Plugin.register :table_it do
   name 'TableIt'
   author '1000ideas'
   description 'Watch your issues right from redmine home page'
-  version '0.1.1'
+  version '0.2.0'
   url 'https://github.com/1000ideas/table_it.git'
   author_url 'http://1000i.pl'  
 
   
   project_module :table_it do
-    # permission :home, { :table_it => [:table_it] },:public => true
     permission :time_actions, issues: :time
     permission :poke, issues: :poke
     permission :close_reopen_issue, issues: :close
