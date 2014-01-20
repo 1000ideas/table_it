928b05dd15069d3b897a8a1de873f1f83469971a
Bartek Bulat <bartek@1000i.pl>
2014-01-20 09:02:33 +0100

    Add custom field configuration

--- init.rb
+++ init.rb
@@ -7,11 +7,11 @@ Redmine::Plugin.register :table_it do
   name 'TableIt'
   author '1000ideas'
   description 'Watch your issues right from redmine home page'
-  version '0.2.1'
+  version '0.2.2'
   url 'https://github.com/1000ideas/table_it.git'
   author_url 'http://1000i.pl' 
 
-  settings default: {default_users: true}, partial: 'settings/table_it'
+  settings default: {default_users: true, end_time: true}, partial: 'settings/table_it'
 
   project_module :table_it do
     permission :time_actions, issues: :time
