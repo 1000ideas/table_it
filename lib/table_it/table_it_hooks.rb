40fcd942500266006feae38a9eaffdac29d742c7
Szczepan Bułat <szczepan@1000i.pl>
2017-02-16 14:55:40 +0100

    refs #19519 check if user attach file when they mentioned it

--- lib/table_it/table_it_hooks.rb
+++ lib/table_it/table_it_hooks.rb
@@ -1,5 +1,6 @@
 module TableIt
   class TableItHooks < Redmine::Hook::ViewListener
     render_on :view_layouts_base_body_bottom, partial: 'table_it/on_close'
+    render_on :view_issues_form_details_bottom, partial: 'table_it/check_attachments'
   end
 end
